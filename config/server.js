b5bbe0af457121fa5f129e4ab416f733318a41c5
gomxx2 <gomgom223@gmail.com>
2022-09-05 15:57:43 +0700

    first commmit yall

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '29af218c993ae9ccb2fbebb95127d772'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1c998702-87f5-40b2-ad2f-4772467e1f78'
+};
